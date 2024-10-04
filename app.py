f348100995ed36e4bcffe4dd2d8b888e073a671a
Vishwaradhya <69788666+vmaradhya2020@users.noreply.github.com>
2024-10-04 19:22:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def main():
     st.write("Enter your question below:")
 
     # Input atext box for user
-    user_input = st.text_input("You:", "")
+    user_input = st.text_area("How can I assist you today? ", height=150)
 
     # Button to send input to chatbot
     if st.button("Submit"):
