1eb3851dadf36d320cff5d0aa99dd14f81a55245
Vishwaradhya <69788666+vmaradhya2020@users.noreply.github.com>
2024-10-04 19:35:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def chatbot(prompt):
 
 # Streamlit app starts here
 def main():
-    st.title("GPT Chatbot - Mavenir Billing Team")
+    st.title("GPT Chatbot - Billing Team")
     st.write("Enter your question below:")
 
     # Input atext box for user
